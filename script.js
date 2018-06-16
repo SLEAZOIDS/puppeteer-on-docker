18f84a6f1881e0fb56c785793468e2f6385302ac
SLEAZOIDS <vegetarian304@gmail.com>
2018-06-17 01:26:14 +0900

    png save dir

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ const puppeteer = require('puppeteer');
     ]
   });
   const page = await browser.newPage();
+  await page.goto(config.url);
 
   // basic認証
   await page.setExtraHTTPHeaders({
     Authorization: `Basic ${new Buffer(`${config.basic_auth_user}:${config.basic_auth_password}`).toString('base64')}`
   });
 
-  await console.log('basic');
+  await console.log('access');
+  await page.screenshot({path: 'png/access.png', fullPage: true});
 
   browser.close();
 })();
